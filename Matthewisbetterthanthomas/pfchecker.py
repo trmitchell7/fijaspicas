6c6c402464608b81ddbb9fbf70468801d2f0c2eb
MKinsman <matthew@kinsman.net>
2015-03-09 14:06:41 -0700

    broke out into methods
    also added further error handling

--- Matthewisbetterthanthomas/pfchecker.py
+++ Matthewisbetterthanthomas/pfchecker.py
@@ -1,35 +1,64 @@
 #!/usr/bin/python3
 
-answer = input("\n\nPlease enter correct answer: ")
+answer = 0
+guess = 0
+tries = 1
 
-def checkAnswer():
-    question = input("\n\nPlease enter your guess: ")
-    if len(question) == 4 and question.isdigit():
+def inputAnswer():
+    global answer
+    answer = input("\n\nPlease enter correct answer: ")
+    return answer
+
+def inputGuess():
+    global guess
+    guess = input("\n\nPlease enter your guess: ")
+    return guess
+
+def errorChecking(x):
+    if len(x) == 4 and x.isdigit():
+        duplicates = 0
+
+        for e in x:
+            for p in range(4):
+                if e == x[p]:
+                    duplicates += 1
+        if duplicates == 4:
+            return True
+        else:
+            return False
+
+    else:
+        return False
+
+def checkAnswer(x, y):
         fija = 0
         pica = 0
+
         for i in range(4):
-            if answer[i] == question[i]:
+            if x[i] == y[i]:
                 fija += 1
 
-        for n in answer:
+        for n in x:
             for p in range(4):
-                if n == question[p]:
+                if n == y[p]:
                     pica += 1
 
         pica = pica - fija
 
-        print ("Guess: " + ''.join(str(e) for e in question) + "\tFijas: " + str(fija) + "\tPicas: " + str(pica))
+        print ("Guess: " + y + "\tFijas: " + str(fija) + "\tPicas: " + str(pica))
         return fija
 
-    else:
-        print ("Guess needs to be 4 numbers, please try entering your guess again. \n \n")
-        return checkAnswer()
-
-def playGame():
-    tries = 1
-    while checkAnswer() < 4:
-        tries += 1
+def play():
+    global tries
+    while errorChecking(inputAnswer()) == False:
+        print ("Answer needs to be 4 nonrepeating numbers, please try entering the answer again.")
+    while errorChecking(inputGuess()) == False:
+        print ("Guess needs to be 4 nonrepeating numbers, please try entering your guess again.")
+    while checkAnswer(answer, guess) < 4:
+        while errorChecking(inputGuess()) == False:
+            print ("Guess needs to be 4 nonrepeating numbers, please try entering your guess again.")
         print ("Guess again!")
-    print ("You won in " + str(tries) + " tries!")
+        tries += 1
+    print ("You won in %s tries!" % (tries))
 
-playGame()
+play()
